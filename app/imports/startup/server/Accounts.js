7eb7d429ef258ec1387ce1296fc8dfc9f78ce976
mcalamasa <calamasa@hawaii.edu>
2019-11-20 21:41:52 -1000

    First and last name saved to user
    Got first and last name to save to a user when signing up. Updated default data to have first and last names

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,10 +4,14 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role) {
+function createUser(email, password, firstName, lastName, role) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
+    profile: {
+      firstName: firstName,
+      lastName: lastName,
+    },
     email: email,
     password: password,
   });
@@ -20,7 +24,9 @@ function createUser(email, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    Meteor.settings.defaultAccounts.map(
+        ({ email, firstName, lastName, password, role }) => createUser(email, firstName, lastName, password, role),
+    );
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
