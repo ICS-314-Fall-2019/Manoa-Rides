5b1fd18ecf13f0552dcbe2bc42c556e18e25dd59
Dylan Decker <dylaned@hawaii.edu>
2019-12-08 18:11:20 -1000

    Create Forgot Password page
    Image from: https://pixabay.com/photos/sea-beach-sunset-hawaii-woman-931164/
    - Set up the structure for implementation
    - Set a new background image

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -16,6 +16,7 @@ import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import ForgotPassword from '../pages/ForgotPassword';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -28,6 +29,7 @@ class App extends React.Component {
               <Route exact path="/" component={Landing}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
+              <Route path="/forgot" component={ForgotPassword}/>
               <ProtectedRoute path="/list" component={ListRides}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/addProfile" component={AddProfile}/>
